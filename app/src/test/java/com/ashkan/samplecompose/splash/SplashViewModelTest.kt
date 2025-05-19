d209463c11f81682bea530c0d47ad45ff787962f
Ashkan <ashkan.k.9877@gmail.com>
2025-05-19 22:55:32 +0200

    more test cases added

--- app/src/test/java/com/ashkan/samplecompose/splash/SplashViewModelTest.kt
+++ app/src/test/java/com/ashkan/samplecompose/splash/SplashViewModelTest.kt
@@ -2,21 +2,17 @@ package com.ashkan.samplecompose.splash
 
 import com.ashkan.samplecompose.MainDispatcherRule
 import com.ashkan.samplecompose.data.cache.DataStoreManager
+import com.ashkan.samplecompose.data.core.NetworkExceptions
 import com.ashkan.samplecompose.data.model.splash.AppConfigModel
 import com.ashkan.samplecompose.data.repository.splash.SplashRepository
-import com.ashkan.samplecompose.ui.screen.splash.SplashAction
 import com.ashkan.samplecompose.ui.screen.splash.SplashViewModel
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.kotlin.mock
@@ -34,6 +30,14 @@ class SplashViewModelTest {
     private val mockedSplashRepository: SplashRepository = mock()
     private val successSplashResponseModel: AppConfigModel = mock()
     private val mockedDataStoreManager: DataStoreManager = mock()
+    private val mockedException: NetworkExceptions = mock()
+
+    @Test
+    fun splashState_ifTokenNotSaved_navigatesToLogin() = runTest{
+        whenever(mockedDataStoreManager.isTokenSaved()).thenReturn(false)
+        viewModel = SplashViewModel(mockedDataStoreManager, mockedSplashRepository)
+        assertTrue(viewModel.stateValue.first().navigateToLogin)
+    }
 
     @Test
     fun splashState_whenTokenSaved_appConfigCalled() = runTest {
@@ -55,11 +59,19 @@ class SplashViewModelTest {
     }
 
     @Test
-    fun splashState_whenApiSuccess_thenResultIsSuccess() = runTest {
+    fun splashState_whenApiSuccess_navigatesToHome() = runTest {
         whenever(mockedSplashRepository.getAppConfig()).thenReturn( flow { emit(Result.success(successSplashResponseModel)) })
         whenever(mockedDataStoreManager.isTokenSaved()).thenReturn(true)
         viewModel = SplashViewModel(mockedDataStoreManager, mockedSplashRepository)
         assertTrue(viewModel.stateValue.first().navigateToHome)
         assertEquals(viewModel.stateValue.first().appConfigFailureMessage, null)
+    }
+
+    @Test
+    fun splashState_whenApiFailed_errorMessageAdded() = runTest {
+        whenever(mockedSplashRepository.getAppConfig()).thenReturn( flow { emit(Result.failure(mockedException)) })
+        whenever(mockedDataStoreManager.isTokenSaved()).thenReturn(true)
+        viewModel = SplashViewModel(mockedDataStoreManager, mockedSplashRepository)
+        assertTrue(viewModel.stateValue.first().appConfigFailureMessage.isNullOrEmpty().not())
     }
 }
